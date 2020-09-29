cadab40d9bdb4cd62483451120dca69aac0a547b
Jan Lipmann <jan.lipmann@likomp.pl>
2020-09-29 09:06:44 +0200

    [CLEAR-50] added example app

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Jan Lipmann on 29/09/2020.
+//  Copyright © 2020 ClearCode. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Jan Lipmann on 29/09/2020.
+//  Copyright © 2020 ClearCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
