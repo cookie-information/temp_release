3f98bf52d93fae68799c8094112e50d164ab667a
Sebastian <seb.osinski@gmail.com>
2021-02-11 19:02:13 +0100

    [CLEAR-77] Replace label with textView. Test support for HTML

--- MobileConsents/UI/Cells/ContentTableViewCell.swift
+++ MobileConsents/UI/Cells/ContentTableViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 final class ContentTableViewCell: UITableViewCell {
-    private let label = UILabel()
+    private let textView = UITextView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -22,26 +22,91 @@ final class ContentTableViewCell: UITableViewCell {
     }
     
     func setText(_ text: String) {
-        label.text = text
+//        textView.text = text
+        
+        let htmlString = "<a href=\"https://www.apple.com\">Visit website</a> The data collected includes:<br><ul><li>Your <b>shoe</b> size</li></ul>"
+        
+        textView.setHTMLText(htmlString)
     }
     
     private func setup() {
-        label.numberOfLines = 0
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.font = .systemFont(ofSize: 18)
+        textView.delegate = self
+        textView.linkTextAttributes = [:]
         
         contentView.backgroundColor = .white
         
-        contentView.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(textView)
+        textView.translatesAutoresizingMaskIntoConstraints = false
         
-        let bottomConstraint = label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+        let bottomConstraint = textView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
         
         bottomConstraint.priority = .defaultHigh
         
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            textView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
             bottomConstraint,
-            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+            textView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            textView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
         ])
+    }
+}
+
+extension ContentTableViewCell: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        print(URL)
+        
+        return false
+    }
+}
+
+extension NSAttributedString {
+     static func fromHTML(_ html: String) -> NSAttributedString? {
+        guard let data = html.data(using: .utf8) else {
+            return nil
+        }
+        let options: [NSAttributedString.DocumentReadingOptionKey : Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+        
+        return try? NSAttributedString(
+            data: data,
+            options: options,
+            documentAttributes: nil
+        )
+    }
+}
+
+extension UITextView {
+    func setHTMLText(_ htmlText: String) {
+        let htmlTemplate = """
+            <!doctype html>
+            <html>
+              <head>
+                <style>
+                  body {
+                    font-family: -apple-system;
+                    font-size: 12pt;
+                  }
+                  a {
+                    font-weight: bold;
+                    text-decoration: none;
+                    color: black;
+                  }
+                  li {
+                    list-style-position: inside;
+                  }
+                </style>
+              </head>
+              <body>
+                \(htmlText)
+              </body>
+            </html>
+            """
+        
+        attributedText = NSAttributedString.fromHTML(htmlTemplate)
     }
 }
