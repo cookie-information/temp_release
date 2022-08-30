2c23807aa393d68a05c9f545c83fd1e41dcc9c95
Andras Marczell <amz@cookieinformation.com>
2022-08-30 17:18:02 +0200

    added privacy policy detail view

---
+++ MobileConsents/UI/PopUp/PrivacyPolicyDetail.swift
@@ -0,0 +1,35 @@
+import UIKit
+import WebKit
+
+internal class PrivacyPolicyDetail: UIViewController {
+    
+    private lazy var webView: WKWebView = {
+        let view = WKWebView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setup()
+    }
+    
+    private func setup() {
+        view.addSubview(webView)
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
