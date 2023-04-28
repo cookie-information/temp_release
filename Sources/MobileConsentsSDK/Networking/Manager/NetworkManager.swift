55d7898b1a3331fc7b317edd19cad5583997412b
Andras Marczell <amz@cookieinformation.com>
2023-04-28 10:13:06 +0200

    fixed clientid and secret being hardcoded

--- Sources/MobileConsentsSDK/Networking/Manager/NetworkManager.swift
+++ Sources/MobileConsentsSDK/Networking/Manager/NetworkManager.swift
@@ -82,8 +82,8 @@ final class NetworkManager {
     }
     
     func authorize(_ completion: @escaping (Result<AuthResponse, Error>) -> Void) {
-        provider.request(.authorize(clientID: "68290ff1-da48-4e61-9eb9-590b86d9a8b9",
-                                    clientSecret: "bfa6f31561827fbc59c5d9dc0b04bdfd9752305ce814e87533e61ea90f9f8da8743c376074e372d3386c2a608c267fe1583472fe6369e3fa9cf0082f7fe2d56d")) { data, response, error in
+        provider.request(.authorize(clientID: clientID,
+                                    clientSecret: clientSecret)) { data, response, error in
             let decoder = JSONDecoder()
           decoder.keyDecodingStrategy = .convertFromSnakeCase
           guard let data = data else {
